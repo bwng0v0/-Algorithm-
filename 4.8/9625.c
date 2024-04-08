aa8c91ee8fe9d0265371dd262a3edf47d9cfd909
bwng0v0 <ehs4742@gmail.com>
2024-04-08 15:36:47 +0900

    md file update

--- 4.8/9625.c
+++ 4.8/9625.c
@@ -5,12 +5,12 @@ int main(){
 int N;scanf("%d",&N);
 
 int arr[N+1][2];
-arr[0][0] = 1;
-arr[0][1] = 0;
+arr[0][0] = 1;//A
+arr[0][1] = 0;//B
 
 for( int i=1; i<N+1; i++ ){
-    arr[i][0] = arr[i-1][1];
-    arr[i][1] = arr[i-1][0]+arr[i-1][1];
+    arr[i][0] = arr[i-1][1];// f(n) = 의 A의 개수는 f(n-1)의 B의 개수
+    arr[i][1] = arr[i-1][0]+arr[i-1][1];// f(n) = 의 B의 개수는 f(n-1)의 A개수+B개수
 }
 
 printf("%d %d",arr[N][0],arr[N][1]);
